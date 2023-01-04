4e3ab474213cbe79c8410fd18014c230522c0b21
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-01-04 20:21:40 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/leonardocosta/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.leonardocosta.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
