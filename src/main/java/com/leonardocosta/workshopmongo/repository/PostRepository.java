9b536aea888c1497781ab3a5c0b0c0c41b4a7454
Leonardo Costa <leonardo.costa.santos.si@gmail.com>
2023-01-06 20:44:01 -0300

    Consulta simples com @Query

--- src/main/java/com/leonardocosta/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/leonardocosta/workshopmongo/repository/PostRepository.java
@@ -3,12 +3,16 @@ package com.leonardocosta.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.leonardocosta.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
